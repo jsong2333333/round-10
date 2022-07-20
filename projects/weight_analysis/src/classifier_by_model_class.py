55e1d507e4d6a95af9d44a7aa6487b484feb2cb8
jsong2333333 <jsong@wpi.edu>
2022-07-20 00:37:59 -0700

    update prediction method based on model class

---
+++ projects/weight_analysis/src/classifier_by_model_class.py
@@ -0,0 +1,71 @@
+from sklearn.metrics import log_loss
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.calibration import CalibratedClassifierCV
+from tqdm import tqdm
+import numpy as np
+from argparse import ArgumentParser
+import logging
+import torch
+import feature_extractor as fe
+from joblib import load
+
+def bootstrap_performance(X, y, n=10, test_size=.2, eps=.01):
+    all_cross_entropy, all_accuracy = [], []
+    for i in tqdm(range(n)):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=i)
+        
+        base_clf = RandomForestClassifier(n_estimators=2000, max_depth=2, criterion='log_loss',  bootstrap=True, random_state=i)
+        clf = CalibratedClassifierCV(base_estimator=base_clf, cv=5)
+        
+        clf.fit(X_train, y_train)
+        
+        all_cross_entropy.append(log_loss(y_test, clf.predict_proba(X_test), eps=eps))
+        all_accuracy.append(clf.score(X_test, y_test))
+    return {'cross_entropy': all_cross_entropy, 
+            'accuracy': all_accuracy}
+
+# Only used for checking the performance
+# X_A = np.load('features_A.npy')
+# X_B = np.load('features_B.npy')
+# y_A = np.load('labels_A.npy')
+# y_B = np.load('labels_B.npy')
+
+# pA = bootstrap_performance(X_A, y_A, n=50)
+# pB = bootstrap_performance(X_B, y_B, n=50)
+# ce_A, acc_A = pA['cross_entropy'], pA['accuracy']
+# ce_B, acc_B = pB['cross_entropy'], pB['accuracy']
+
+# print('mean ce_A: ', sum(ce_A)/len(ce_A), 'mean ce_B: ', sum(ce_B)/len(ce_B))
+# print('mean acc_A: ', sum(acc_A)/len(acc_A), 'mean acc_B: ', sum(acc_B)/len(acc_B))
+# print(ce_A, acc_A, ce_B, acc_B)
+
+
+def weight_analysis_detector(model_filepath):
+    predict_model_class_and_features = fe.get_model_features(model_filepath)
+    predict_model_class = predict_model_class_and_features['model_class']
+    predict_model_features = np.asarray([predict_model_class_and_features['features']])
+    if predict_model_class == 'A':
+        clf = load('classifier_model_A.joblib')
+    elif predict_model_class == 'B':
+        clf = load('classifier_model_B.joblib')
+    else:
+        logging.warning('No able to detect such model class')
+        return
+    predict_probability = clf.predict_proba(predict_model_features)
+    logging.info('Trojan Probability of this class {} model is: {}'.format(predict_model_class, predict_probability[0, -1]))
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Weight Analysis Classifier')
+    parser.add_argument('--model_filepath', type=str, help='File path to the pytorch model file to be evaluated.')
+
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s")
+    logging.info("weight analysis classifier launched")
+
+    if args.model_filepath is not None:
+        logging.info('Calling the weight analysis classifier')
+        weight_analysis_detector(args.model_filepath)
+
--- projects/weight_analysis/src/feature_extractor.py
+++ projects/weight_analysis/src/feature_extractor.py
@@ -2,7 +2,9 @@ import torch
 import os
 import numpy as np
 import pandas as pd
-import itertools
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.calibration import CalibratedClassifierCV
+from joblib import dump
 
 DATA_PATH = '/scratch/data/TrojAI/round10-train-dataset/'
 
@@ -11,7 +13,7 @@ def get_all_features():
     num_models = 144
     for model_num in range(num_models):
         model_filepath = _get_model_filepath(model_num)
-        all_features.append(get_model_features(model_filepath))
+        all_features.append(get_model_features(model_filepath)['features'])
 
     X = np.asarray(all_features)
     return X
@@ -23,30 +25,62 @@ def get_all_labels():
     return y
 
 
-def get_model_features(model_filepath):
+def get_features_and_labels_by_model_class():
+    model_A_features, model_B_features, model_A_labels, model_B_labels = [], [], [], []
+    num_models = 144
+    metadata_filepath = os.path.join(DATA_PATH, "METADATA.csv")
+    metadata = pd.read_csv(metadata_filepath)
+    labels = metadata['poisoned'].to_numpy()
+    for model_num in range(num_models):
+        model_filepath = _get_model_filepath(model_num)
+        class_and_features_by_model = get_model_features(model_filepath)
+        model_class, model_features = class_and_features_by_model['model_class'], class_and_features_by_model['features']
+        if model_class == 'A':
+            model_A_features.append(model_features)
+            model_A_labels.append(labels[model_num])
+        elif model_class == 'B':
+            model_B_features.append(model_features)
+            model_B_labels.append(labels[model_num])
+    return {'model_A_features': np.asarray(model_A_features), 'model_B_features': np.asarray(model_B_features), 
+            'model_A_labels': np.asarray(model_A_labels), 'model_B_labels': np.asarray(model_B_labels)}
 
+
+def get_model_features(model_filepath):
     model = torch.load(model_filepath)
     model_backbone = model.backbone
+
+    num_of_params = sum(p.numel() for p in model.parameters())/1000.0
 
     all_backbone_params = []
     for param in model_backbone.parameters():
         all_backbone_params.append(param.data.cpu().numpy())
 
+    model_class, features = '', []
+    if num_of_params == 41755.2860:  # model A
+        model_class = 'A'
+        features = _get_eigen_vals(all_backbone_params, 1, 3)
+    elif num_of_params == 35641.8260:  # model B
+        model_class = 'B'
+        features = _get_eigen_vals(all_backbone_params, 0, 4)
 
+    return {'model_class': model_class, 'features': features}
+
+
+def _get_eigen_vals(all_backbone_params, idx_low=0, idx_high=3):
     features = []
     num_layers = 0
     for backbone_params in all_backbone_params:
         if len(backbone_params.shape) > 2:
-            reshaped_params = backbone_params.reshape(backbone_params.shape[1], -1)
-            _, singular_values, _ = np.linalg.svd(reshaped_params,False)
-            squared_singular_values = singular_values**2
-            top_five_sq_sv = squared_singular_values[:5]
-            features += top_five_sq_sv.tolist()
+            if num_layers >= idx_low and num_layers <= idx_high:
+                reshaped_params = backbone_params.reshape(backbone_params.shape[1], -1)
+                _, singular_values, _ = np.linalg.svd(reshaped_params,False)
+                squared_singular_values = singular_values**2
+                # top_five_sq_sv = squared_singular_values[:5]
+                features += squared_singular_values.tolist()
+                num_layers += 1
             num_layers += 1
-        if num_layers == 2:
-            break
-
     return features
+
 
 def _get_model_filepath(model_num: int) -> os.path:
     num_as_str = str(100000000 + model_num)[1:]
@@ -54,8 +88,15 @@ def _get_model_filepath(model_num: int) -> os.path:
     return os.path.join(DATA_PATH, model_id, 'model.pt')
 
 if __name__ == "__main__":
-    X = get_all_features()
-    y = get_all_labels()
-
-    np.save('features.npy', X)
-    np.save('labels.npy', y)
+    all_features_and_labels = get_features_and_labels_by_model_class()
+    model_A_features, model_B_features, model_A_labels, model_B_labels = all_features_and_labels['model_A_features'], all_features_and_labels['model_B_features'], all_features_and_labels['model_A_labels'], all_features_and_labels['model_B_labels']
+    # np.save('features_A', model_A_features)
+    # np.save('features_B', model_B_features)
+    # np.save('labels_A', model_A_labels)
+    # np.save('labels_B', model_B_labels)
+    base_clf = RandomForestClassifier(n_estimators=2000, max_depth=2, criterion='log_loss',  bootstrap=True, random_state=0)
+    clf_A, clf_B = CalibratedClassifierCV(base_estimator=base_clf, cv=5), CalibratedClassifierCV(base_estimator=base_clf, cv=5)
+    clf_A.fit(model_A_features, model_A_labels)
+    clf_B.fit(model_B_features, model_B_labels)
+    dump(clf_A, 'classifier_model_A.joblib')
+    dump(clf_B, 'classifier_model_B.joblib')
