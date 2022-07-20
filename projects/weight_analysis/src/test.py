161fe64583133f7a0c2bc8a00b3cddcab2c35adf
jsong2333333 <jsong@wpi.edu>
2022-07-20 00:44:52 -0700

    changing test.py file

--- projects/weight_analysis/src/test.py
+++ projects/weight_analysis/src/test.py
@@ -3,114 +3,88 @@ import numpy as np
 import itertools
 
 
+import pandas as pd
+df=pd.read_csv('/scratch/jialin/round-10/projects/weight_analysis/src/METADATA.csv', sep=',',header=None)
 
 
-store = []
-store_eigs = []
+store_eigs_modelA = []
+store_eigs_modelB = []
 
-nmodels=140
-for i in range(nmodels):
-    if i<10:
-        path = '/scratch/data/TrojAI/round10-train-dataset/id-0000000'+str(i)+'/model.pt'
-    elif i<100:
-        path = '/scratch/data/TrojAI/round10-train-dataset/id-000000'+str(i)+'/model.pt' 
-    elif i>100:
-        path = '/scratch/data/TrojAI/round10-train-dataset/id-00000'+str(i)+'/model.pt' 
+targets_modelA = []
+targets_modelB = []
 
-    m = torch.load(path)
-    print(path)
-    #print(m)
-    idx=1
-    #print(m.head.regression_head)
-    
-    #model_sub = m
-    #model_sub = m.backbone
-    #model_sub = m.backbone.extra
-    #model_sub = m.head.classification_head
-    #model_sub = m.head.regression_head
-    model_sub = m.backbone
-    #print(model_sub)
 
+def get_eigs(model_sub, idx_low=0, idx_high=3):
     output = []
 
     for param in model_sub.parameters():
         output.append(param.data.cpu().numpy())
-            
-    print(output[idx].shape)            
-
-    def eigs(x):
-        _,s,_=np.linalg.svd(x, False)
-        return s
-
-    #s = [eigs(output[idx][i,:,:,:].reshape(512,9)) for i in range(16)]
-    #s = list(itertools.chain.from_iterable(s))
-
-    
     k = 0
-    metric = []
     eigs = []
     for o in output:
         if len(o.shape)>2:
-            x = o.reshape(o.shape[1],-1)
-            print(x.shape)
-
-            _,s,_ = np.linalg.svd(x,False)
-            s = s**2
-
-    #        #print(np.asarray(s).min())
-            metric.append(np.asarray(s[0:10]).sum())
-            eigs.append(s[0:5])
-            #print(s.shape)
+            if k >= idx_low and k <= idx_high:  
+                x = o.reshape(o.shape[1], -1)
+                _, s, _ = np.linalg.svd(x, False)
+                eigs.append(s[0::]**2)
             k += 1
-        if k == 2: 
-            break
+    return np.asarray(list(itertools.chain(*eigs))).flatten()
 
-    store.append(np.asarray(metric).mean())
-    store_eigs.append(np.asarray(list(itertools.chain(*eigs))).flatten())
-    #print(np.asarray(store_eigs).shape)
+
+
+nmodels = 143
+for i in range(nmodels):
+    model_id = df[0][i]
+    path = '/scratch/data/TrojAI/round10-train-dataset/' + str(model_id) + '/model.pt'
+    m = torch.load(path)
     
+    pars = sum(p.numel() for p in m.parameters())/1000.0
+    if pars == 41755.2860:
+        model_sub = m.backbone # select a block
+        
+        targets_modelA.append(df[1][i]) # get target
+        store_eigs_modelA.append(get_eigs(model_sub, idx_low=1, idx_high=3)) # get features
+
+    elif pars == 35641.8260:
+
+        model_sub = m.backbone
+
+        targets_modelB.append(df[1][i]) # get target
+        store_eigs_modelB.append(get_eigs(model_sub, idx_low=0, idx_high=4)) # get features
+
+
+
+### Build Classifier 
+### ---------------------------------------------------------------------------
+
+def classify(features, targets):
+    from sklearn.metrics import log_loss
+    from sklearn.ensemble import RandomForestClassifier
+
+    X = features
+    y = targets
+    split = int(X.shape[0]*0.6)
+    clf = RandomForestClassifier(n_estimators=2000, max_depth=2, criterion='log_loss', bootstrap=True, random_state=0)
+    clf.fit(X[0:split,:],y[0:split])
+
+    #print(log_loss(y, clf.predict_proba(X)[:,1]))
+    print('Cross-Entropy train: ', log_loss(y[0:split], clf.predict_proba(X[0:split,:])[:,1]))
+    print('Cross-Entropy test: ', log_loss(y[split::], clf.predict_proba(X[split::,:])[:,1]))
     
-    #metric = []
-    #for o in output:
-    #    if len(o.shape)>2:
-            #print(len(o.shape))
-    #        metric.append(np.abs(o).mean())
+    ## save model here
+    #return None
 
-    #store.append(np.asarray(metric).max())
-    #print(store[-1])
+print('***** ******')
+print('***** ******')
 
-#print(store)
-store = np.asarray(store)
-#store[:] = 0
-#print(store)
+print('***** Model A ******')
+targets_modelA = np.asarray(targets_modelA)
+store_eigs_modelA = np.asarray(store_eigs_modelA)
+#print(store_eigs_modelA.shape)
+classify(store_eigs_modelA, targets_modelA)
 
-store_eigs = np.asarray(store_eigs)
-print(store_eigs.shape)
-
-
-
-import pandas as pd
-df=pd.read_csv('METADATA.csv', sep=',',header=None)
-targets = df[1].to_numpy()
-#print(targets)
-
-from sklearn.metrics import log_loss
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-
-X = store_eigs#.reshape(nmodels,-1)
-y = targets[0:nmodels]
-
-print(X.shape)
-print(y.shape)
-
-clf = RandomForestClassifier(n_estimators=1000, max_depth=2, criterion='log_loss',  random_state=0)
-clf.fit(X[0:80,:],y[0:80])
-
-print(log_loss(y, clf.predict_proba(X)[:,1]))
-print(log_loss(y[0:80], clf.predict_proba(X[0:80,:])[:,1]))
-print(log_loss(y[80:140], clf.predict_proba(X[80:140,:])[:,1]))
-
-
-
-#print(clf.predict_proba(store.reshape(50,-1)))
+print('***** Model B ******')
+targets_modelB = np.asarray(targets_modelB)
+store_eigs_modelB = np.asarray(store_eigs_modelB)
+#print(store_eigs_modelB.shape)
+classify(store_eigs_modelB, targets_modelB)
