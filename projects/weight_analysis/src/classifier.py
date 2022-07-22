67218be44b0cdc3bf9ab14b0288238564ac2ed0d
root <root@ip-172-31-34-215.us-west-2.compute.internal>
2022-07-22 09:20:42 +0000

    slightly changed of code for building container

--- projects/weight_analysis/src/classifier.py
+++ projects/weight_analysis/src/classifier.py
@@ -8,13 +8,15 @@ from argparse import ArgumentParser
 import logging
 import torch
 import feature_extractor as fe
+from joblib import load
 
-def bootstrap_performance(X, y, n=50, test_size=.2, eps=.01):
+
+def bootstrap_performance(X, y, n=10, test_size=.2, eps=.01):
     all_cross_entropy, all_accuracy = [], []
     for i in tqdm(range(n)):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=i)
         
-        base_clf = RandomForestClassifier(n_estimators=1000, max_depth=2, criterion='log_loss',  random_state=i)
+        base_clf = RandomForestClassifier(n_estimators=2000, max_depth=2, criterion='log_loss',  bootstrap=True, random_state=i)
         clf = CalibratedClassifierCV(base_estimator=base_clf, cv=5)
         
         clf.fit(X_train, y_train)
@@ -24,22 +26,35 @@ def bootstrap_performance(X, y, n=50, test_size=.2, eps=.01):
     return {'cross_entropy': all_cross_entropy, 
             'accuracy': all_accuracy}
 
-X = np.load('src/features.npy')
-y = np.load('src/labels.npy')
+# Only used for checking the performance
+# X_A = np.load('features_A.npy')
+# X_B = np.load('features_B.npy')
+# y_A = np.load('labels_A.npy')
+# y_B = np.load('labels_B.npy')
 
-# performance = bootstrap_performance(X, y)
-# all_cross_entropy, all_accuracy = performance['cross_entropy'], performance['accuracy']
+# pA = bootstrap_performance(X_A, y_A, n=50)
+# pB = bootstrap_performance(X_B, y_B, n=50)
+# ce_A, acc_A = pA['cross_entropy'], pA['accuracy']
+# ce_B, acc_B = pB['cross_entropy'], pB['accuracy']
 
-# TODO: write output somewhere and save the classifier
+# print('mean ce_A: ', sum(ce_A)/len(ce_A), 'mean ce_B: ', sum(ce_B)/len(ce_B))
+# print('mean acc_A: ', sum(acc_A)/len(acc_A), 'mean acc_B: ', sum(acc_B)/len(acc_B))
+# print(ce_A, acc_A, ce_B, acc_B)
+
 
 def weight_analysis_detector(model_filepath):
-    base_clf = RandomForestClassifier(n_estimators=1000, max_depth=2, criterion='log_loss',  random_state=0)
-    clf = CalibratedClassifierCV(base_estimator=base_clf, cv=5)
-    clf.fit(X, y)
-
-    predict_model_features = np.asarray([fe.get_model_features(model_filepath)])
+    predict_model_class_and_features = fe.get_model_features(model_filepath)
+    predict_model_class = predict_model_class_and_features['model_class']
+    predict_model_features = np.asarray([predict_model_class_and_features['features']])
+    if predict_model_class == 'A':
+        clf = load('/classifier_model_A.joblib')
+    elif predict_model_class == 'B':
+        clf = load('/classifier_model_B.joblib')
+    else:
+        logging.warning('No able to detect such model class')
+        return
     predict_probability = clf.predict_proba(predict_model_features)
-    logging.info('Trojan Probability of this model is: {}'.format(predict_probability[0, -1]))
+    logging.info('Trojan Probability of this class {} model is: {}'.format(predict_model_class, predict_probability[0, -1]))
 
 
 if __name__ == '__main__':
